403232cff6cd2f2719f2d5f6e0dd16f34b675add
chrisfishbob <56414801+chrisfishbob@users.noreply.github.com>
2021-01-28 18:25:37 -0800

    Update PrimeChecker3.py
    Spacing fixes

--- PrimeChecker3.py
+++ PrimeChecker3.py
@@ -7,8 +7,7 @@ def main():
     test_factor = 2  # Initializes 2 as the first test case, since we don't need to test 1
     test_limit = test_number
     factor_amount = 0  # This number indicates the amount of factor that the test number has
-    factors = [1,
-               test_number]  # 1 and the test number is included because all ints are divisible by 1 and itself
+    factors = [1, test_number]  # 1 and the test number is included because all ints are divisible by 1 and itself
 
     while test_factor < test_limit:
         if test_number % test_factor == 0:
@@ -16,8 +15,7 @@ def main():
             test_limit = test_number / test_factor  # Test limit is also a factor
             factors.append(int(test_limit))
         else:
-            test_limit = math.ceil((
-                                               test_number / test_factor))  # Eliminates numbers that could not possibly be factors
+            test_limit = math.ceil((test_number / test_factor))  # Eliminates numbers that could not possibly be factors
 
         test_factor += 1  # Moves on to the next test factor
 
